3c61114747cb2ade2aae8e5b373e450e5bd03cb8
Colin Purtell <cpurtell13@gmail.com>
2025-12-14 12:28:41 -0500

    Completed R script to clean and merged datasets for analysis

--- 01_data_prep.R
+++ 01_data_prep.R
@@ -3,8 +3,37 @@ library(tidyverse)
 library(lubridate)
 library(janitor)
 
-# Read in the datasets from data/
-pirates_raw <- read_csv("data/pirates_subset.csv", show_col_types = FALSE)
-pirates <- pirates_raw %>% clean_names()
-weather_raw <- read_csv("data/weather_subset.csv", show_col_types = FALSE)
-weather <- weather_raw %>% clean_names()
+# Read in the datasets from data folder, then standardize for merging
+pirates <- read_csv("data/pirates_subset.csv", show_col_types = FALSE) %>% clean_names()
+weather <- read_csv("data/weather_subset.csv", show_col_types = FALSE) %>% clean_names()
+
+pirates$day <- mdy(pirates$day)
+weather$day <- mdy(weather$day)
+
+pirates_clean <- pirates %>%
+  transmute(
+    day,
+    wait   = average_wait_time_daily,
+    season = factor(season),
+    month  = month,
+    year   = year
+  ) %>%
+  filter(!is.na(wait))
+
+weather_clean <- weather %>%
+  transmute(
+    day,
+    mean_temp = mean_temp,
+    precip    = precipitation
+  )
+
+pw_merged <- pirates_clean %>%
+  left_join(weather_clean, by = "day") %>%
+  mutate(
+    month = case_when(
+      is.numeric(month) ~ month.name[month],
+      TRUE ~ as.character(month)
+    ),
+    month = factor(month, levels = month.name),
+    season = fct_drop(season)
+  )
